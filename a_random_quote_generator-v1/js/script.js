91946b169bf01c82b484acbe86eaed7c5a5ee866
Norman Lew <normanlew@users.noreply.github.com>
2020-02-11 16:26:42 -0800

    Update script.js
    This code fulfills the requirements for the assignment.

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -3,31 +3,82 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+// This program generates random quotes from famous people
 
 /*** 
- * `quotes` array 
+ * This array contains objects of quotes.
 ***/
+quotes = [
+  {
+    quote: "The roots of education... are bitter, but the fruit is sweet.",
+    source: "Aristotle"
+  },
+
+  {
+    quote: "In matters of conscience, the law of majority has no place.",
+    source: "Gandhi",
+    citation: "Young India",
+    year: 1920
+  },
+
+  {
+    quote: "Spread love everywhere you go; first of all in your house. Give love to your children, to your wife or husband, to a next door neighbor. Let no one ever come to you without leaving better and happier. Be the living expression of God's kindness; kindness in your face, kindness in your eyes, kindness in your smile.",
+    source: "Mother Teresa"
+  },
+
+  {
+    quote: "It is better to fail in originality, than to succeed in imitation. He who has never failed somewhere, that man can not be great. Failure is the true test of greatness.",
+    source: "Herman Melville",
+    citation: "Hawthorne and His Mosses",
+    year: 1850
+  },
+
+  {
+    quote: "A person who's happy will make others happy; a person who has courage and faith will never die in misery!",
+    source: "Anne Frank",
+    citation: "The Diary of a Young Girl",
+    year: "1942-1944"
+  }
+];
 
 
 
 /***
- * `getRandomQuote` function
+ * This function selects a quote from the array of quote objects.
 ***/
+function getRandomQuote() {
+  var index = Math.floor(Math.random() * 5);
+  return quotes[index];
+}
+
+
+/***
+ * This function creates an html string that contains all the attributes of a quote object.  It then updates the html file with the
+ * new quote.
+***/
+function printQuote() {
+  var theQuote = getRandomQuote();
+  var html = "<p class='quote'>" + theQuote.quote + "</p><p class='source'>" + theQuote.source;
+
+  if (theQuote.hasOwnProperty('citation')) {
+    html += "<span class='citation'>" +  theQuote.citation + "</span>";
+  };
+
+  if (theQuote.hasOwnProperty('year')) {
+    html += "<span class='year'>" + theQuote.year +  "</span>";
+  }
+
+  html += "</p>";
+
+  document.getElementById('quote-box').innerHTML = html; 
+}
+
+
 
 
 
 /***
- * `printQuote` function
-***/
-
-
-
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
+ * This function calls the printQuote() function everytime the button that generates a random quote is clicked
 ***/
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
